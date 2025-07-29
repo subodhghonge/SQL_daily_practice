331e231058814b8be599b81ee05c8261a69badf3
subodhghonge <subodhghonge12@gmail.com>
2025-07-29 12:25:58 +0530

    Queries

--- Day 3/table2_queries.sql
+++ Day 3/table2_queries.sql
@@ -8,3 +8,11 @@ GROUP BY b.building;
 --Q2. Find the list of all buildings and their capacity
 SELECT Building_name, Capacity
 FROM Buildings;
+
+--Q3.List all buildings and the distinct employee roles in each building (including empty buildings)
+SELECT b.building_name, e.role
+FROM buildings b
+LEFT JOIN employees e
+ON b.building_name = e.building
+GROUP BY b.building_name, e.role
+ORDER BY b.building_name;
