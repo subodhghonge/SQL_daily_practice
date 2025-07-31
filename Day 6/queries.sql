330547d48a95b3f28971c19951591b6fa3df9511
subodhghonge <subodhghonge12@gmail.com>
2025-07-31 11:43:49 +0530

    queries practice

--- Day 6/queries.sql
+++ Day 6/queries.sql
@@ -25,3 +25,30 @@ WHERE height > 160 AND weight > 70;
 SELECT first_name, last_name, allergies
 FROM patients
 WHERE allergies IS NOT NULL AND city = 'Hamilton'; 
+
+--DIFFICULTY = MEDIUM
+--Q7 Show unique birth years from patients and order them by ascending.
+SELECT DISTINCT YEAR(birth_date) AS Birth_years
+FROM patients
+ORDER BY Birth_years ASC;
+
+--Q8 Show unique first names from the patients table which only occurs once in the list.
+-- For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
+SELECT first_name
+FROM patients
+GROUP BY first_name
+HAVING count(*) = 1;
+
+--Q9 Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+SELECT patient_id, first_name
+FROM patients
+WHERE first_name LIKE 's%s' and LEN(first_name) >= 6;
+
+--Q10 Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'. Primary diagnosis is stored in the admissions table.
+SELECT a.patient_id, a.first_name, a.last_name
+FROM patients as a
+left JOIN admissions AS b
+ON a.patient_id = b.patient_id
+WHERE b.diagnosis = 'Dementia';
+
+--Q11 
