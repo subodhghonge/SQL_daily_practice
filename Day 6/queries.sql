12df7c2d5aae86fcfbf4bd5bf39262967a862cd0
subodhghonge <subodhghonge12@gmail.com>
2025-07-31 12:26:28 +0530

    queries practice

--- Day 6/queries.sql
+++ Day 6/queries.sql
@@ -51,4 +51,31 @@ left JOIN admissions AS b
 ON a.patient_id = b.patient_id
 WHERE b.diagnosis = 'Dementia';
 
---Q11 
+--Q11 Display every patient's first_name. Order the list by the length of each name and then by alphabetically.
+SELECT first_name
+FROM patients
+ORDER BY LEN(first_name), first_name ASC;
+
+--Q12 Show the total amount of male patients and the total amount of female patients in the patients table. Display the two results in the same row.
+SELECT 
+	COUNT(CASE WHEN gender = "M" THEN 1 END) AS male_patients,
+    COUNT(CASE WHEN gender = "F" THEN 1 END) AS female_patients
+FROM patients;
+
+--Q13 Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
+SELECT first_name, last_name, allergies
+FROM patients
+WHERE allergies = 'Penicillin' OR allergies = 'Morphine'
+ORDER BY allergies, first_name, last_name;
+
+--Q14 Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
+SELECT 
+    patient_id, 
+    diagnosis
+FROM 
+    admissions
+GROUP BY 
+    patient_id, 
+    diagnosis
+HAVING 
+    COUNT(*) > 1;
