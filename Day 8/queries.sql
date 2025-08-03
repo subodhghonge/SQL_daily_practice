4edff7187b4ea5492400b17fdcef60c2b5a4bbf5
subodhghonge <subodhghonge12@gmail.com>
2025-08-03 14:51:49 +0530

    Queries

--- Day 8/queries.sql
+++ Day 8/queries.sql
@@ -25,3 +25,30 @@ ON a.doctor_id = b.attending_doctor_id
 group by a.doctor_id;
 
 --Q3 For each doctor, display their id, full name, and the first and last admission date they attended.
+select b.doctor_id, CONCAT(b.first_name, ' ', b.last_name), MIN(a.admission_date) AS first_admission_date, mAX(a.admission_date) AS last_admission_date
+FROM admissions AS a 
+JOIN doctors AS b 
+ON a.attending_doctor_id = b.doctor_id
+GROUP BY b.doctor_id;
+
+--Q4 Display the total amount of patients for each province. Order by descending.
+SELECT b.province_name, COUNT(a.patient_id) AS patient_count
+FROM patients AS a 
+JOIN province_names AS b 
+ON a.province_id = b.province_id
+GROUP BY b.province_name
+ORDER BY patient_count DESC;
+
+--Q5 For every admission, display the patient's full name, their admission diagnosis, and their doctor's full name who diagnosed their problem.
+select 
+	concat(a.first_name, ' ',a.last_name),
+    b.diagnosis,
+    CONCAT(c.first_name, ' ',c.last_name)
+FROM 
+	patients AS a 
+JOIN
+	admissions AS b 
+    ON a.patient_id = b.patient_id
+JOIN
+	doctors AS c 
+    ON b.attending_doctor_id = c.doctor_id;
