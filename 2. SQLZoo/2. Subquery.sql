34baad0699b2a320fd12196dac5576733d6c7612
subodhghonge <subodhghonge12@gmail.com>
2025-09-30 17:04:17 +0530

    Sub Queries

--- 2. SQLZoo/2. Subquery.sql
+++ 2. SQLZoo/2. Subquery.sql
@@ -20,4 +20,29 @@ WHERE continent IN (
 )
 ORDER BY name;
 
---4. 
+--4. Which country has a population that is more than United Kingdom but less than Germany? Show the name and the population.
+SELECT name, population
+FROM world
+WHERE population > (SELECT population FROM world WHERE name = "United Kingdom") 
+AND population < (SELECT population FROM world WHERE name = "Germany");
+
+--5. Germany (population roughly 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+-- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+-- The format should be Name, Percentage for example:
+-- name	    percentage
+-- Albania	3%
+-- Andorra	0%
+-- Austria	11%
+-- ...	...
+-- Decimal places
+-- Percent symbol %
+
+SELECT name,
+       CONCAT(ROUND(100 * population / (
+           SELECT population
+           FROM world
+           WHERE name = 'Germany'
+       ), 0), '%') AS percentage
+FROM world
+WHERE continent = 'Europe'
+ORDER BY name;
