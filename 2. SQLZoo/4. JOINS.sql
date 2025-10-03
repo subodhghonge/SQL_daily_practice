c8a16a215dcf4e37e9d2f8ad8e2a7bff528dfc6e
subodhghonge <subodhghonge12@gmail.com>
2025-10-03 16:52:52 +0530

    Joins Query

---
+++ 2. SQLZoo/4. JOINS.sql
@@ -0,0 +1,6 @@
+--1. show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
+SELECT matchid, player FROM goal 
+WHERE teamid = 'GER';
+
+
+--2. 
